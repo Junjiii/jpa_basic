41dcdd55b3c352abd592883d26be731d2e2055cd
Jjunjiii’ <drum1701@gmail.com>
2024-08-04 21:23:42 +0900

    Example 3 : Delivery & Category

---
+++ src/main/java/hellojpa/jpashop/domain/Delivery.java
@@ -0,0 +1,21 @@
+package hellojpa.jpashop.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+}
